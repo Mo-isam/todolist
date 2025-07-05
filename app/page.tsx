f0aa632a419d5a891c233f1829e2db24316ae2d6
Mo-isam <mia199728@gmail.com>
2025-07-05 03:34:09 +0300

    05/07/2025

--- app/page.tsx
+++ app/page.tsx
@@ -1,96 +1,42 @@
-"use client"
+"use client";
 import "./page.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash, faCheck, faUndo } from "@fortawesome/free-solid-svg-icons";
-import React, { useRef, useState } from "react";
-
-interface TodoItem {
-    id: number;
-    text: string;
-    isDone: boolean;
-}
+import React, { useEffect, useRef, useState } from "react";
+import WeatherCard from "./weathercard";
 
 function App() {
-    const [todoList, setTodoList] = useState<TodoItem[]>([
-        { id: 1, text: "Example task", isDone: false },
-    ]);
-    const [inputValue, setInputValue] = useState("");
-    const inputRef = useRef<HTMLInputElement>(null);
-
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInputValue(e.target.value);
-    };
-
-    const addTodo = () => {
-        if (inputValue.trim()) {
-            setTodoList((prev) => [
-                ...prev,
-                { id: Date.now(), text: inputValue, isDone: false },
-            ]);
-            setInputValue("");
-            inputRef.current?.focus();
+    const [weather, setweather] = useState<any>();
+    const [weatherlist, setweatherlist] = useState<any>();
+    const axios = require("axios");
+    async function getUser() {
+        try {
+            const response = await axios.get(
+                "https://api.openweathermap.org/data/2.5/weather?q=Cairo,eg&APPID=9d7d987f5fb87b1606271d16db0becfa&units=metric"
+            );
+            console.log(response.data);
+            setweather(response.data);
+        } catch (error) {
+            console.error(error);
         }
-    };
-
-    const removeTodo = (id: number) => {
-        setTodoList((prev) => prev.filter((todo) => todo.id !== id));
-    };
-
-    const toggleTodo = (id: number) => {
-        setTodoList((prev) =>
-            prev.map((todo) =>
-                todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
-            )
-        );
-    };
-
-    const handleKeyPress = (e: React.KeyboardEvent) => {
-        if (e.key === "Enter") {
-            addTodo();
+    }
+    async function getweather() {
+        try {
+            const response = await axios.get(
+                "https://api.openweathermap.org/data/2.5//forecast?q=Cairo,eg&APPID=9d7d987f5fb87b1606271d16db0becfa&units=metric"
+            );
+            console.log(response);
+            setweatherlist(response.data)
+        } catch (error) {
+            console.error(error);
         }
-    };
+    }
 
+    useEffect(() => {
+        getweather();
+        getUser();
+    }, []);
     return (
         <div className="app-container">
-            <h1>Todo App</h1>
-            <div className="todo-container">
-                {todoList.map((todo) => (
-                    <div
-                        key={todo.id}
-                        className={`todo-item ${todo.isDone ? "done" : ""}`}
-                    >
-                        <p>{todo.text}</p>
-                        <div className="actions">
-                            <FontAwesomeIcon
-                                icon={todo.isDone ? faUndo : faCheck}
-                                onClick={() => toggleTodo(todo.id)}
-                                className={`action-icon ${
-                                    todo.isDone ? "undo" : "complete"
-                                }`}
-                            />
-                            <FontAwesomeIcon
-                                icon={faTrash}
-                                onClick={() => removeTodo(todo.id)}
-                                className="action-icon delete"
-                            />
-                        </div>
-                    </div>
-                ))}
-            </div>
-
-            <div className="input-container">
-                <input
-                    ref={inputRef}
-                    value={inputValue}
-                    onChange={handleInputChange}
-                    onKeyPress={handleKeyPress}
-                    type="text"
-                    placeholder="Enter a new task..."
-                />
-                <button onClick={addTodo} disabled={!inputValue.trim()}>
-                    Add Task
-                </button>
-            </div>
+            {weather && weatherlist&& <WeatherCard currentWeatherData={weather} forecastData={weatherlist} />}
         </div>
     );
 }
